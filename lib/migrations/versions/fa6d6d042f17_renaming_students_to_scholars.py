3372f459e1ec54b957cfcd517d32cf228047fbbf
Reagan Ongaya <reagan.ongaya@student.moringaschool.com>
2023-12-15 11:02:37 +0300

    Assignment

---
+++ lib/migrations/versions/fa6d6d042f17_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: fa6d6d042f17
+Revises: 791279dd0760
+Create Date: 2023-12-15 10:59:36.356485
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fa6d6d042f17'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- lib/models.py
+++ lib/models.py
@@ -17,7 +17,7 @@ engine = create_engine('sqlite:///migrations_test.db')
 Base = declarative_base()
 
 class Student(Base):
-    __tablename__ = 'students'
+    __tablename__ = 'scholars'
 
     id = Column(Integer(), primary_key=True)
     name = Column(String(), index=True)
@@ -27,6 +27,6 @@ class Student(Base):
     enrolled_date = Column(DateTime(), default=datetime.now())
 
     def __repr__(self):
-        return f"Student {self.id}: " \
+        return f"Scholars {self.id}: " \
             + f"{self.name}, " \
             + f"Grade {self.grade}"
